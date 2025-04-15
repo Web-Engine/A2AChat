1c0482a35447b171c3c135314d5ab6061ecf0fc7
Web-Engine <gsts007@gmail.com>
2025-04-16 04:57:19 +0900

    feat(client): React Query 설정 추가

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,25 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 1,
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 
 root.render(
   <React.StrictMode>
-  <App />
-</React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  </React.StrictMode>
 );
